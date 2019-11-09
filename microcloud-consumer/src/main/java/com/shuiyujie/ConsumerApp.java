7dad4e20d599f8d083473338c5d755bd49346f1e
Yujie Shui <shuiyujie.cn@gmail.com>
2019-11-09 23:17:00 +0800

    Ribbon基本使用

--- microcloud-consumer/src/main/java/com/shuiyujie/ConsumerApp.java
+++ microcloud-consumer/src/main/java/com/shuiyujie/ConsumerApp.java
@@ -2,12 +2,14 @@ package com.shuiyujie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author shui
  * @create 2019-11-09
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApp.class,args);
